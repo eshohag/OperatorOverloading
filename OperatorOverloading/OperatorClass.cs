69d495a2c22890c8dd5d3732e9f58ff0ff31e4ab
mdshohagmia <shohaghassan14@hotmail.com>
2019-01-15 23:34:09 +0600

    Initial Commit

---
+++ OperatorOverloading/OperatorClass.cs
@@ -0,0 +1,20 @@
+﻿namespace OperatorOverloading
+{
+    class OperatorClass
+    {
+        public int Salary { get; set; }
+        public static OperatorClass operator +(OperatorClass first, OperatorClass second)
+        {
+            OperatorClass aOperatorClass = new OperatorClass();
+            aOperatorClass.Salary = first.Salary + second.Salary;
+            return aOperatorClass;
+        }
+
+        public static OperatorClass operator -(OperatorClass first, OperatorClass second)
+        {
+            OperatorClass aOperatorClass = new OperatorClass();
+            aOperatorClass.Salary = first.Salary - second.Salary;
+            return aOperatorClass;
+        }
+    }
+}
--- OperatorOverloading/Program.cs
+++ OperatorOverloading/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OperatorOverloading
 {
@@ -10,6 +6,18 @@ namespace OperatorOverloading
     {
         static void Main(string[] args)
         {
+            OperatorClass operatorClass1 = new OperatorClass();
+            operatorClass1.Salary = 50000;
+
+            OperatorClass operatorClass2 = new OperatorClass();
+            operatorClass2.Salary = 30000;
+
+            OperatorClass aOperatorClass = new OperatorClass();
+            aOperatorClass = operatorClass1 - operatorClass2;
+
+            Console.WriteLine(aOperatorClass.Salary);
+
+            Console.ReadKey();
         }
     }
 }
